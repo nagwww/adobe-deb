19232bb010bdf1c5b42cfacfb007f2baef3b852c
Nag <nagwww@gmail.com>
2023-05-18 11:38:15 -0700

    Update post-install.sh

--- config/scripts/post-install.sh
+++ config/scripts/post-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "This is a post install script"
-echo "Welcome to Adobe : Demo to Daniel/Todd " > /usr/share/nginx/html/index.html
+echo "Welcome to Adobe : Demo for test " > /usr/share/nginx/html/index.html
